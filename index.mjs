0c9327a6f47e582c43c48a6f37935d17b1559525
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-04-28 21:09:24 +0530

    saved

--- index.mjs
+++ index.mjs
@@ -2,13 +2,18 @@ import express from "express";
 import userRouter from "./api/user.mjs";
 import converstationRouter from "./api/converstation.mjs";
 import messageRouter from "./api/message.mjs";
+import cors from 'cors'
 
 const server = express();
 server.use(express.json());
+server.use(cors())
 
 server.use("/api/users", userRouter);
 server.use("/api/conversation", converstationRouter);
 server.use("/api/message", messageRouter);
+server.get("/test", (req, res) => {
+  res.json({ message: "hello" });
+});
 
 server.listen(4000, () => {
   console.log("server is running...");
